76fbb413330eb692e9cb2f99911f3e8d1ecaa89c
Dunzan <manifestpositivity@gmail.com>
2026-06-01 14:33:29 +0700

    Update ConfigDropdown.jsx

--- components/ConfigDropdown.jsx
+++ components/ConfigDropdown.jsx
@@ -177,7 +177,7 @@ export default function ConfigDropdown({
             <div className="flex flex-col gap-2 border border-black bg-white p-2.5">
               <div className="flex items-center justify-between gap-2">
                 <span id="countdown-toggle-label" className="text-sm font-medium">
-                  Auto refresh
+                  Auto Refresh
                 </span>
                 <ToggleSwitch
                   checked={draftCountdownEnabled}
